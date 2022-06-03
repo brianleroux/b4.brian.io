77f546686a1291682880106898ccb1571e0177a4
Brian LeRoux <b@brian.io>
2022-06-02 22:45:16 -0700

    fixes for the render and the res

--- src/http/get-auth/03-render.mjs
+++ src/http/get-auth/03-render.mjs
@@ -2,11 +2,11 @@ import view from '@architect/views/auth.mjs'
 
 export default async function render (req) {
   let website = req.query.client_id
-  let scopes = req.query.scope.split(' ').filter(Boolean)
+  let scopes = req.query.scope?.split(' ').filter(Boolean)
   let meta = req.meta
   let state = req.query.state
   let redirect_uri = req.query.redirect_uri
   return {
-    html: view({ website, scopes, meta, state })
+    html: view({ website, scopes, meta, state, redirect_uri })
   }
 }
--- src/views/auth.mjs
+++ src/views/auth.mjs
@@ -2,10 +2,10 @@ import layout from './layout.mjs'
 
 export default function renderauth ({ website, scopes, meta, state, redirect_uri }) {
   
-  let logo = meta.logo? `<img src=${meta.logo}>` : ''
+  let logo = meta?.logo? `<img src=${meta.logo}>` : ''
 
   let scope = ''
-  if (scopes.length > 0) {
+  if (scopes?.length > 0) {
     scope += `This application will be able to:`
     for (let s of scopes) {
       msg += `<li><label for=${s}>${s}</label> <input type=checkbox value=${s}>`
@@ -15,7 +15,7 @@ export default function renderauth ({ website, scopes, meta, state, redirect_uri
   let body = `
     <header>
       ${ logo }
-      <h1>Authorize ${meta.name || website} to use your account?</h1>
+      <h1>Authorize ${meta?.name || website} to use your account?</h1>
     </header>
     <form class=card-auth action=/approve method=post>
       <input type=hidden name=redirect_uri value=${ redirect_uri }>
@@ -23,7 +23,6 @@ export default function renderauth ({ website, scopes, meta, state, redirect_uri
       ${ scope }
       <button> ✅ Authorize</button>
     </form>
-    <pre style=display:none;>${JSON.stringify({ meta, scopes }, null, 2)}</pre>
   `
 
   return layout({ body, page:'auth' })
