13f7a8b81047518726a1bb85cfec58ed9ca1fb46
Brian LeRoux <b@brian.io>
2022-06-06 14:45:10 -0700

    move validation into http

--- src/events/webmention-receive/index.mjs
+++ src/events/webmention-receive/index.mjs
@@ -1,6 +1,6 @@
 import arc from '@architect/functions'
 import tiny from 'tiny-json-http'
-//import getWebmentionEndpoint from './_get-webmention-endpoint.mjs'
+import { mf2 } from 'microformats-parser'
 
 export let handler = arc.events.subscribe(receive)
 
@@ -8,11 +8,6 @@ async function receive (payload) {
 
   console.log('received webmention', JSON.stringify(payload, null, 2))
 
-  // ensure target and source exist and are different
-  let { target, source } = payload
-  if (!target) return
-  if (!source) return
-  if (target === source) return
 
   // attempt to read the target
   let targetReq = false 
--- src/http/post-webmention/index.mjs
+++ src/http/post-webmention/index.mjs
@@ -1,6 +1,27 @@
 import arc from '@architect/functions'
 
-export let handler = arc.http.async(fn)
+export let handler = arc.http.async(log, valid, fn)
+
+async function log (req) {
+  console.log(JSON.stringify(req, null, 2))
+}
+
+async function valid (req) {
+  let { target, source } = req.body
+  let errors = []
+  if (!target)
+    errors.push('missing_target')
+  if (!source)
+    errors.push('missing_source')
+  if (target === source)
+    errors.push('invalid: source must not match target')
+  if (errors.length > 0) {
+    return {
+      code: 400,
+      json: { errors }
+    }
+  }
+}
 
 async function fn (req) {
   await arc.events.publish({
