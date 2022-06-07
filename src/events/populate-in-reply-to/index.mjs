ff24d731b0b1b82875709324cdbabe2af2da4208
Brian LeRoux <b@brian.io>
2022-06-07 15:22:28 -0700

    smarter and simpler reply context parsing

--- src/events/populate-in-reply-to/index.mjs
+++ src/events/populate-in-reply-to/index.mjs
@@ -6,39 +6,66 @@ import { update } from '@architect/shared/notes.mjs'
 
 export let handler = arc.events.subscribe(populateInReplyTo)
 
+// { icon?, author?, content?, published?, url }
 async function populateInReplyTo ({ entryID, content, ttl, name }) {
 
   // log the incoming payload
   console.log(JSON.stringify({ entryID, content, ttl, name }, null, 2))
 
   // parse content for hrefs
-  let hrefs = getHrefs(content)
-
-  for (let href of hrefs) {
+  let baseUrl = getHrefs(content)[0]
+  if (baseUrl) {
     try {
-
-      // attempt to discover the webmention endpoint
-      let { body } = await tiny.get({ url: href })
-
-      // if not found look for html <link>
-      let { rels, items } = mf2(body, { baseUrl: href })
-
-      if (Array.isArray(items)) {
-        console.log('found items', items)
-        let hentry = items.find(i => i.type.includes('h-entry'))
-        let hcard = items.find(i => i.type.includes('h-card'))
-        if (hcard || hentry) {
-          let result = await update({
-            entryID,
-            context: { hentry, hcard }
-          })
-          console.log(result)
-        }
-      }
+      let context = await read({ baseUrl })
+      let result = await update({ entryID, context })
     }
     catch (e) {
       // log failure and continue
       console.log('swallowing', e) 
     }
   }
+}
+
+// looking for {icon, author, content, published, url }
+async function read ({ baseUrl }) {
+  let { body } = await tiny.get({ url:baseUrl })
+  let { items, rels } = result = mf2(body, { baseUrl })
+
+  // look for icon
+  let icon = rels.icon? rels.icon[0] : false
+
+  // look for author
+  let hcard = items.find(o => o.type.includes('h-card'))
+  let author = false
+  if (hcard) {
+    if (hcard.properties.name) author = hcard.properties.name[0]
+  }
+
+  // look for note content
+  let content = false
+  let url = false
+  let published = false
+  let hentry = items.find(o => o.type.includes('h-entry'))
+  if (hentry) {
+    if (hentry.properties.content) content = hentry.properties.content[0].value
+    if (hentry.properties.url) url = hentry.properties.url[0]
+    if (hentry.properties.published) published = hentry.properties.published[0]
+    if (hentry.properties.author) {
+      if (typeof hentry.properties.author[0] === 'string') {
+        author = hentry.properties.author[0]
+      }
+      let ref = hentry.properties.author[0]
+      if (typeof ref === 'object' && ref.type[0] === 'h-card' ) {
+        author = hentry.properties.author[0].properties.name[0]
+      }
+    }
+  }
+
+  let res = { url: baseUrl }
+  if (icon) res.icon = icon
+  if (author) res.author = author
+  if (content) res.content = content.substr(0, 240)
+  if (url) res.url = url
+  if (published) res.published = published
+  return res
 }
---
+++ src/views/_in-reply-to.mjs
@@ -0,0 +1,14 @@
+export default function inReplyTo ({ icon, author, content, published, url }) {
+  let img = icon && author? `<img src=${ icon } alt=${ author }>` : ''
+  let name = author? `<p class="p-author h-card">${ author }</p>` : ''
+  let preview = content? `<p class="p-content">${ content }</p>` : ''
+  let when = published? `<time class="dt-published">${ published }</time>` : ''
+  let where = `<a class="u-url" href="${ url }">${ when || '⚓︎' }</a>`
+  return `<div class="u-in-reply-to h-cite">
+  <strong>↳ In reply to</strong>
+  ${ img }
+  ${ name } 
+  ${ preview } 
+  ${ where }
+ </div>`
+}
